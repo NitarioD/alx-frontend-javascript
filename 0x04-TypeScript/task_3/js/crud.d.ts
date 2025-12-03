3803736d05b19c8db16f6514d2fe05fd9c49477a
Jola <nitariod@gmail.com>
2025-12-03 23:19:48 +0100

    attempt to fix task 3 to meet test requirement 3

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,6 +1,6 @@
 import { RowID, RowElement } from "./interface";
 
-declare function insertRow(row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
 declare function readRow(id: RowID): RowElement | null;
 declare function updateRow(id: RowID, element: RowElement): boolean;
 declare function deleteRow(id: RowID): boolean;
