b7da027a68541e11deeaba54452ad3e256bf1500
Jola <nitariod@gmail.com>
2025-12-03 23:11:13 +0100

    attempt to fix StudentClass task requirement

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,19 +24,13 @@ function printTeacher(firstName: string, lastName: string): string {
   return `${firstName}. ${lastName}`;
 }
 
-interface StudentClassInterface {
+class StudentClass {
   firstName: string;
   lastName: string;
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-interface StudentClassConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
-}
-
-class StudentClass implements StudentClassInterface {
-  constructor(public firstName: string, public lastName: string) {}
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
   workOnHomework(): string {
     return "Currently working";
