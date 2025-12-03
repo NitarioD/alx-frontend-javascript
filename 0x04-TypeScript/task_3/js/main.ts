36e14ad3d4ac42487f415b71f3c9935c7f7e4d2e
Jola <nitariod@gmail.com>
2025-12-03 23:18:37 +0100

    attempt to fix task 3 to meet test requirement 2

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -10,10 +10,8 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+// @pretty-ignore
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
