60414aafa574fe5acf6cd28225abea6765a70d73
Jola <nitariod@gmail.com>
2025-12-03 22:44:54 +0100

    attempt to fix task_2 createEmployee test not passing

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,7 +34,7 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+export function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   }
